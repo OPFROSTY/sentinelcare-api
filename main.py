63664d3b312cb434316c9e1b748888c86abb281a
OPFROSTY <151564793+OPFROSTY@users.noreply.github.com>
2026-01-08 15:09:29 -0400

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+import os
+from supabase import create_client, Client
 
 app = FastAPI()
+
+# Carrega variáveis de ambiente
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 @app.get("/")
 def home():
@@ -9,3 +17,11 @@ def home():
 @app.get("/ping")
 def ping():
     return {"ping": "pong"}
+
+@app.get("/test-db")
+def test_db():
+    response = supabase.table("users").select("*").limit(1).execute()
+    return {
+        "connected": True,
+        "data": response.data
+    }
